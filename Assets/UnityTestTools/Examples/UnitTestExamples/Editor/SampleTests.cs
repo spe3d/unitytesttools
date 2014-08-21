7d19c420e9c0fd1c8eaa2e53f1061d4a35d2d2a1
Elvis Alistar <elvis@unity3d.com>
2014-08-21 18:07:43 +0200

    Remove unused using directives

--- Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
-using UnityEngine;
 
 namespace UnityTest
 {
--- Assets/UnityTestTools/UnitTesting/Editor/TestRunner/UnitTestInfo.cs
+++ Assets/UnityTestTools/UnitTesting/Editor/TestRunner/UnitTestInfo.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using NUnit.Core;
-using UnityEngine;
 using Object = System.Object;
 
 namespace UnityTest
