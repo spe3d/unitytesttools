ec8d89c287665056c2adab134f47889dd9f4f2e3
Richard Fine <richardf@unity3d.com>
2015-01-10 08:07:55 +0000

    Removed leftover settings from UnitTestsRunnerSettings

--- Assets/UnityTestTools/UnitTesting/Editor/UnitTestsRunnerSettings.cs
+++ Assets/UnityTestTools/UnitTesting/Editor/UnitTestsRunnerSettings.cs
@@ -7,19 +7,10 @@ namespace UnityTest
 
     public class UnitTestsRunnerSettings : ProjectSettingsBase
     {
-        public bool optionsFoldout;
-        public bool filtersFoldout;
         public bool runOnRecompilation;
         public bool horizontalSplit = true;
         public bool autoSaveSceneBeforeRun;
         public bool runTestOnANewScene;
-
-        public int categoriesMask;
-        public string testFilter = "";
-        public bool showFailed = true;
-        public bool showIgnored = true;
-        public bool showNotRun = true;
-        public bool showSucceeded = true;
 
         public void ToggleRunTestOnANewScene() {
             runTestOnANewScene = !runTestOnANewScene;
