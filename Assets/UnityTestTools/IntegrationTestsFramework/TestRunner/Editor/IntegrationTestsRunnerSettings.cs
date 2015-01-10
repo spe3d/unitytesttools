9232dfbf7c0d9d5432452556fb00488129d66626
Richard Fine <richardf@unity3d.com>
2015-01-10 04:38:48 +0000

    Store i-test window filters in EditorPrefs, not in the project, to eliminate VCS churn

--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/IntegrationTestsRunnerSettings.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/IntegrationTestsRunnerSettings.cs
@@ -1,19 +1,44 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace UnityTest
 {
     public class IntegrationTestsRunnerSettings : ProjectSettingsBase
     {
         public bool showOptions;
-        public string filterString;
+        [NonSerialized] public string filterString;
         public bool showAdvancedFilter;
-        public bool showSucceededTest = true;
-        public bool showFailedTest = true;
-        public bool showNotRunnedTest = true;
-        public bool showIgnoredTest = true;
+		[NonSerialized] public bool showSucceededTest = true;
+		[NonSerialized] public bool showFailedTest = true;
+		[NonSerialized] public bool showNotRunnedTest = true;
+		[NonSerialized] public bool showIgnoredTest = true;
         public bool addNewGameObjectUnderSelectedTest;
         public bool blockUIWhenRunning = true;
+
+		public override void Save ()
+		{
+			// Store the test filter stuff to EditorPrefs, not disk, to avoid VCS churn
+			EditorPrefs.SetString("UnityTest.IntegrationTestsRunnerSettings.Filter", filterString);
+			EditorPrefs.SetBool("UnityTest.IntegrationTestsRunnerSettings.ShowSucceededTest", showSucceededTest);
+			EditorPrefs.SetBool("UnityTest.IntegrationTestsRunnerSettings.ShowFailedTest", showFailedTest);
+			EditorPrefs.SetBool("UnityTest.IntegrationTestsRunnerSettings.ShowNotRunnedTest", showNotRunnedTest);
+			EditorPrefs.SetBool("UnityTest.IntegrationTestsRunnerSettings.ShowIgnoredTest", showIgnoredTest);
+		}
+
+		public void OnEnable()
+		{
+			filterString = EditorPrefs.GetString("UnityTest.IntegrationTestsRunnerSettings.Filter");
+			showSucceededTest = EditorPrefs.GetBool("UnityTest.IntegrationTestsRunnerSettings.ShowSucceededTest");
+			showFailedTest = EditorPrefs.GetBool("UnityTest.IntegrationTestsRunnerSettings.ShowFailedTest");
+			showNotRunnedTest = EditorPrefs.GetBool("UnityTest.IntegrationTestsRunnerSettings.ShowNotRunnedTest");
+			showIgnoredTest = EditorPrefs.GetBool("UnityTest.IntegrationTestsRunnerSettings.ShowIgnoredTest");
+		}
+
+		public void OnDisable()
+		{
+			Save();
+		}
     }
 }
