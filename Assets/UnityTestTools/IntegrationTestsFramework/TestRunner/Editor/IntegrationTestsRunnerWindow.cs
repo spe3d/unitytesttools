9f84aa69124c78b41b073f107fe901c76d5c92f3
Richard Fine <rfine@tbrf.net>
2015-02-24 23:40:22 +0000

    more whitespace...

--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/IntegrationTestsRunnerWindow.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/IntegrationTestsRunnerWindow.cs
@@ -528,7 +528,7 @@ namespace UnityTest
                 {
                     EditorUtility.ClearProgressBar();
                     EditorApplication.isPaused = true;
-               	}
+                }
             }
 
             public void TestRunInterrupted(List<ITestComponent> testsNotRun)
